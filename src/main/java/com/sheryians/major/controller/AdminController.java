435f9fee46d1decbd6cedf54b5e5b1c3900668d8
sachin singh <kbtug17@gmail.com>
2022-12-22 15:32:48 +0530

    Ecommerce code

---
+++ src/main/java/com/sheryians/major/controller/AdminController.java
@@ -0,0 +1,145 @@
+package com.sheryians.major.controller;
+
+
+import com.sheryians.major.dto.ProductDTO;
+import com.sheryians.major.model.Category;
+import com.sheryians.major.model.Product;
+import com.sheryians.major.service.CategoryService;
+import com.sheryians.major.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+
+@Controller
+public class AdminController {
+
+    public static String uploadDir = System.getProperty("user.dir") + "/src/main/resources/static/productImages";
+    @Autowired
+    CategoryService categoryService;
+
+    @Autowired
+    ProductService productService;
+
+    //Admin Home Page
+    @GetMapping("/admin")
+    public String adminHome() {
+        return "adminHome";
+    }
+
+    // Category Page
+    @GetMapping("/admin/categories")
+    public String getCategory(Model model) {
+
+        model.addAttribute("categories", categoryService.getAllCategory());
+        return "categories";
+    }
+
+    @GetMapping("/admin/categories/add")
+    public String getAddCategory(Model model) {
+        model.addAttribute("category", new Category());
+        return "categoriesAdd";
+    }
+
+    @PostMapping("/admin/categories/add")
+    public String postAddCategory(@ModelAttribute("category") Category category) {
+        categoryService.addCategory(category);
+        return "redirect:/admin/categories";
+    }
+
+    @GetMapping("/admin/categories/delete/{id}")
+    public String deleteCategory(@PathVariable int id) {
+        categoryService.deleteCategory(id);
+        return "redirect:/admin/categories";
+    }
+
+    @GetMapping("/admin/categories/update/{id}")
+    public String updateCategory(@PathVariable int id, Model model) {
+
+        Optional<Category> category = categoryService.getCategoryById(id);
+        if (category.isPresent()) {
+
+            model.addAttribute("category", category.get());
+            return "categoriesAdd";
+        } else
+            return "404";
+    }
+
+    // Product Page
+    @GetMapping("/admin/products")
+    public String getProduct(Model model) {
+
+        model.addAttribute("products", productService.getALLProduct());
+        return "products";
+    }
+
+    @GetMapping("/admin/products/add")
+    public String getAddProduct(Model model) {
+        model.addAttribute("productDTO", new ProductDTO());
+        model.addAttribute("categories", categoryService.getAllCategory());
+        return "productsAdd";
+    }
+
+    @PostMapping("/admin/products/add")
+    public String postAddProduct(@ModelAttribute("productDTO") ProductDTO productDTO,
+                                 @RequestParam("productImage") MultipartFile file,
+                                 @RequestParam("imgName") String imgName) throws IOException {
+        //ProductDto to Product Object
+
+        Product product = new Product();
+        product.setId(productDTO.getId());
+        product.setName(productDTO.getName());
+        product.setCategory(categoryService.getCategoryById(productDTO.getCategoryId()).get());
+        product.setPrice(productDTO.getPrice());
+        product.setWeight(productDTO.getWeight());
+        product.setDescription(productDTO.getDescription());
+        String imageUUID;
+
+        if (!file.isEmpty()) {
+            imageUUID = file.getOriginalFilename();
+            Path fileNameAndPath = Paths.get(uploadDir, imageUUID);
+            Files.write(fileNameAndPath, file.getBytes());
+        } else {
+            imageUUID = imgName;
+        }
+        product.setImageName(imageUUID);
+        productService.addProduct(product);
+
+        return "redirect:/admin/products";
+    }
+
+    @GetMapping("/admin/product/delete/{id}")
+    public String deleteProduct(@PathVariable long id) {
+        productService.deleteProduct(id);
+        return "redirect:/admin/products";
+    }
+
+    @GetMapping("/admin/product/update/{id}")
+    public String updateProduct(@PathVariable long id, Model model) {
+
+        //Product to ProductDTO Object
+
+        Product product = productService.getProductById(id).get();
+        ProductDTO productDTO = new ProductDTO();
+        productDTO.setId(product.getId());
+        productDTO.setName(product.getName());
+        productDTO.setCategoryId(product.getCategory().getId());
+        productDTO.setDescription(product.getDescription());
+        productDTO.setPrice(product.getPrice());
+        productDTO.setWeight(product.getWeight());
+        productDTO.setImageName(product.getImageName());
+
+        model.addAttribute("categories", categoryService.getAllCategory());
+        model.addAttribute("productDTO", productDTO);
+
+        return "productsAdd";
+    }
+
+}
---
+++ src/main/java/com/sheryians/major/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.sheryians.major.repository;
+
+import com.sheryians.major.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role,Integer> {
+
+
+}
